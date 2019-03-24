712ad4a5f8b86492fb90863b8fa8870bf268c5bf
elitan <johan@eliasson.me>
2019-03-24 19:18:30 +0100

    first test

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # hbp-js-sdk
+
+
+## usage
+
+```
+const config = {
+  endpoint: 'http://localhost:8083',
+};
+
+const hbp = new hbp(config);
+```
+
+### examples
+
+```
+const res = await hbp.upload(path, files, onUploadProgress);
+const url = hbp.url(file_key);
+```
---
+++ index.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+class hbp {
+  constructor(config) {
+    this.endpoint = config.endpoint;
+  }
+
+  async upload(path, files, onUploadProgress = false) {
+
+    let form_data = new FormData();
+
+    files.forEach(file => {
+      form_data.append('files', file);
+    });
+
+    const upload_res = await axios.post(`${this.endpoint}/storage/upload`, form_data, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        'x-path': path,
+      },
+      onUploadProgress: onUploadProgress,
+      withCredentials: true,
+    });
+
+    return upload_res.data;
+  }
+
+  url(path) {
+    return `${this.endpoint}/storage/file/${path}`;
+  }
+}
