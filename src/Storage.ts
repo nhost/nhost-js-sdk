d74340b1ea01ca3c4e9ca01464232d358574edca
elitan <johan@eliasson.me>
2020-12-28 10:12:32 +0100

    trying fetch-blob

--- src/Storage.ts
+++ src/Storage.ts
@@ -7,7 +7,7 @@ import {
   utf8Bytes,
   percentEncodedBytes,
 } from "./utils";
-import { Blob } from "blob-polyfill";
+import Blob from "fetch-blob";
 
 export default class Storage {
   private http_client: AxiosInstance;
