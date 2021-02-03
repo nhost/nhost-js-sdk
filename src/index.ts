ef3442ac66c018858bcf295e1edb61fb4d4ba071
elitan <johan@eliasson.me>
2021-02-03 13:43:10 +0100

    Helping error messages for baseURL

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,18 @@ class Nhost {
   }
 
   public initializeApp(config: types.UserConfig) {
+    if ("base_url" in config) {
+      return console.error(
+        "use `baseURL` instead of `base_url` to initiate nhost"
+      );
+    }
+
+    if ("baseUrl" in config) {
+      return console.error(
+        "use `baseURL` (URL is uppercase) instead of `baseUrl` to initiate nhost"
+      );
+    }
+
     this.baseURL = config.baseURL;
     this.appInitialized = true;
     this.useCookies = config.useCookies ? config.useCookies : false;
@@ -36,7 +48,7 @@ class Nhost {
 
   public auth() {
     if (!this.appInitialized || !this.baseURL) {
-      throw "app is not initialized. Call nhost.initializeApp(config). Read more here: https://docs.nhost.io/libraries/nhost-js-sdk#setup.";
+      throw "app is not initialized. Call nhost.initializeApp(config). Read more at: https://docs.nhost.io/libraries/nhost-js-sdk#setup.";
     }
 
     const config = {
